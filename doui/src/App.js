714d2a8c40bfe3f8bd25f2741864ebfbfdbbc113
tuluffy <tuluffy@163.com>
2017-12-24 12:52:21 +0800

    new: add component/alert

--- doui/src/App.js
+++ doui/src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Alert from './components/AlertComp/index';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            titleText: '提示',
+            context: '拦路雨偏似雪花，饮泣的你冻吗？'
+        };
+
+        this.confirm = this.confirm.bind(this);
+        this.$alert = this.$alert.bind(this);
+    };
+
+    $alert() {
+        let maskContainer = document.createElement('div');
+        maskContainer.id = 'maskContainer';
+        document.body.appendChild(maskContainer);
+
+        ReactDOM.render(<Alert titleText={this.state.titleText} context={this.state.context}
+                               cbFn={this.confirm}/>, maskContainer);
+
+    };
+
+    confirm() {
+        alert('wahah');
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <button onClick={this.$alert}>点我弹出层</button>
+            </div>
+        );
+    }
 }
 
 export default App;
